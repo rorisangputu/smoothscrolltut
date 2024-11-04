287ad57a6dde32a28b9d739fa782d53c0033d544
Rorisang Putu <rorisangputu@gmail.com>
2024-11-04 22:58:03 +0200

    adding gsap timelines and references to elements

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -2,11 +2,31 @@ import Image from 'next/image'
 import styles from './styles.module.css'
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useLayoutEffect, useRef } from 'react';
 
 const Index = () => {
+
+  const backgroundImage = useRef(null)
+  const introImage = useRef(null)
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: "tops",
+        end: "+=500px",
+        scrub: true,
+        markers: true
+      }
+    })
+
+    tl.from(backgroundImage.current, { clipPath: "inset(15%)" })
+  }, [])
+
   return (
     <div className={styles.intro}>
-      <div className={styles.backgroundImage}>
+      <div ref={backgroundImage} className={styles.backgroundImage}>
         <Image
           src={'/images/abstract.jpg'}
           fill={true}
@@ -15,7 +35,7 @@ const Index = () => {
       </div>
 
       <div className={styles.introContainer}>
-        <div data-scroll data-scroll-speed='0.3' className={styles.introImage}>
+        <div ref={introImage} data-scroll data-scroll-speed='0.3' className={styles.introImage}>
           <Image
             src={'/images/horizon.jpg'}
             fill={true}
