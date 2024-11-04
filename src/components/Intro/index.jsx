82c6a2f8c3b4c70ec8927fbe2abbcf5436564c9d
Rorisang Putu <rorisangputu@gmail.com>
2024-11-04 22:38:02 +0200

    importing gsap and scrolltrigger

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import styles from './styles.module.css'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const Index = () => {
   return (
