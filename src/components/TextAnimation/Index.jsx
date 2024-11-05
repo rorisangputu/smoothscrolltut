c19644198c9550766d0222b8ad5d5e40609996d4
Rorisang Putu <rorisangputu@gmail.com>
2024-11-05 21:37:47 +0200

    new text animation compo

---
+++ src/components/TextAnimation/Index.jsx
@@ -0,0 +1,17 @@
+import { wrap } from 'gsap'
+import styles from './styles.module.css'
+
+const Index = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.textWrapper}>
+                <p>UI Designer</p>
+                <p>Rapper</p>
+                <p>Business Man</p>
+                <p>King</p>
+            </div>
+        </div>
+    )
+}
+
+export default Index
